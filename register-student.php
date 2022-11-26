df5d7d58e1a896f449c142b4f6603eefb2ceecac
GutoRomagnolo <guromagnolo23@hotmail.com>
2022-11-26 18:58:28 -0300

    feat: preenchendo cadastro de alunos por php

--- register-student.php
+++ register-student.php
@@ -1,24 +1,52 @@
 <?php
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    function get_data() {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  function get_data() {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $identifier = $_POST['identifier'];
+    $gender = $_POST['gender'];
+    $age = $_POST['age'];
+    $address = $_POST['address'];
+    $file_name = 'StudentsData'. '.json';
+
+    if (file_exists("$file_name")) {
+      $current_data = file_get_contents("$file_name");
+      $array_data = json_decode($current_data, true);
+
+      $extra = array(
+        'Name' = $_POST['name'],
+        'Email' = $_POST['email'],
+        'Identifier' = $_POST['identifier'],
+        'Gender' = $_POST['gender'],
+        'Age' = $_POST['age'],
+        'Address' = $_POST['address'],
+      );
+      $array_data[] = $extra;
+      echo "file exist<br/>";
+      return json_encode($array_data);
+    }
+    else {
       $datae = array();
       $datae[] = array(
-        'Name' => $_POST['name'],
-        'Branch' => $_POST['branch'],
-        'College' => $_POST['college']
+        'Name' = $_POST['name'],
+        'Email' = $_POST['email'],
+        'Identifier' = $_POST['identifier'],
+        'Gender' = $_POST['gender'],
+        'Age' = $_POST['age'],
+        'Address' = $_POST['address'],
       );
+      echo "file not exist<br/>";
       return json_encode($datae);
     }
-
-    $name = "gfg";
-    $file_name = $name . '.json';
-    
-    if(file_put_contents(
-      "$file_name", get_data())) {
-        echo $file_name .' file created';
-      }
-    else {
-      echo 'There is some error';
-    }
   }
+
+  $file_name = 'StudentsData'. '.json';
+
+  if (file_put_contents("$file_name", get_data())) {
+    echo 'success';
+  }
+  else {
+    echo 'There is some error';
+  }
+}
 ?>
